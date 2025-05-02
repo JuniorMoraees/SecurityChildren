37829981bf5f344520ea09df5bec3582f6e26b06
JuniorMoraees <juniormoraesdev@gmail.com>
2025-05-01 23:21:52 -0300

    Correção do CORS para que o Angular consiga consultar o Backend

--- src/main/java/br/com/jmtech/configuration/CorsConfig.java
+++ src/main/java/br/com/jmtech/configuration/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://securitychildren.netlify.app/")
+                .allowedOrigins("https://securitychildren.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
